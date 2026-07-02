505cae141929ca9db17fc04483e2db09e5e7f53d
vananh20075-cmyk <297028108+anhlvk25411-sketch@users.noreply.github.com>
2026-07-02 15:37:41 +0700

    Add CSDL_doan

---
+++ CSDL_doan/LoaithucvatEx.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.uic import loadUi  # Sửa từ 'ui' thành 'uic'
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # Load file UI - đảm bảo tên file đúng
+        loadUi('Loaithucvat.ui', self)  # Sửa tên file cho đúng
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
---
+++ CSDL_doan/PhieukhuEx.py
@@ -0,0 +1,16 @@
+# PhieuloaiEx.py
+import sys
+from PyQt6.QtWidgets import QApplication, QDialog
+from PyQt6.uic import loadUi
+
+class MainWindow(QDialog):  # Đổi từ QMainWindow thành QDialog
+    def __init__(self):
+        super().__init__()
+        # Load file UI
+        loadUi('Phieukhu.ui', self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
